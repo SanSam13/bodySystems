35578827c66bd58092d4174207ba866a020c3b12
Sandra <Sandra@MacBook-Pro-de-Mac.local>
2023-11-18 12:04:30 -0600

    configuracion inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#E10A0A',
+        'accent-color': '#3C1053',
+        'text-black': '#333333',
+        'text-gray': '#999999'
+      },
+      fontFamily: {
+        'DMSans': ['DM Sans', 'sans-serif'],
+        'Quicksand': ['Quicksand', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
